d1af1d261039a695721849f07c68de8816201e66
YANGNAVI <yangnavi@naver.com>
2022-05-09 20:16:44 +0900

    master에서 커밋

---
+++ wikipage/src/components/background.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Background(){
+  const templateStyle={
+    height: "800px",
+    width: "1920px",
+    background: "#8ECAFF"
+  }
+  return (
+    <div style={templateStyle}>
+    </div>
+  )
+}
+
+export default Background;
